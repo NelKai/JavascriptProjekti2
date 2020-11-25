d099ab795f35ea9ef8792a995d00bb92b5206811
NelKai <55228995+NelKai@users.noreply.github.com>
2020-11-25 17:39:44 +0200

    Tiedostojen päivitys

--- projekti2.js
+++ projekti2.js
@@ -1,3 +1,4 @@
+
 function getInfo() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/", true);
@@ -5,22 +6,62 @@ function getInfo() {
 
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-           // document.getElementById("info").innerHTML=xmlhttp.responseText;
-           var xmlTxt = xmlhttp.responseXML;
+            var xmlTxt = xmlhttp.responseXML;
+           
            var otsikot = xmlTxt.getElementsByTagName("Title");
-           
-           
-           for (var i = 0; i < otsikot.length; i++) {
-               otsikko = "<br><input type='button' id='btn_" + i + "' value='" + otsikot[i].childNodes[0].nodeValue + "' style='width:300px;'><br>";
-               document.getElementById("info").innerHTML += otsikko;
-           }
+           var teatterit = xmlTxt.getElementsByTagName("Theatre");
+           var jVuodet = xmlTxt.getElementsByTagName("ProductionYear");
+           var pituudet = xmlTxt.getElementsByTagName("LengthInMinutes");
+           var aloitusAjat = xmlTxt.getElementsByTagName("dttmShowStart");
+           var lopetusAjat = xmlTxt.getElementsByTagName("dttmShowEnd");
+           var ikaRajaKuvat = xmlTxt.getElementsByTagName("RatingImageUrl");
+           var showt = xmlTxt.getElementsByTagName("Show");
+           var kuvaURLit = xmlTxt.getElementsByTagName("EventSmallImagePortrait");
+           var teatteriIDt = xmlTxt.getElementsByTagName("TheatreID");
 
+           var elokuvatiedot;
+
+           var data = "<table border='1px solid black' id='datatable'>" +
+                            "<th>Elokuvan nimi</th>" +
+                            "<th>Näytöksen tiedot</th>" + 
+                            "<th>Elokuvan pituus (min)</th>" +
+                            "<th>Ikäraja</th>" + 
+                            "<th></th>";
+            
+            var menu = document.getElementById("dropMenu");
+                        
+           for (var i = 0; i < showt.length; i++) {
+                         
+                elokuvatiedot = {nimi:otsikot[i].childNodes[0].nodeValue,
+                                julkaisuvuosi:jVuodet[i].childNodes[0].nodeValue, 
+                                teatteri:teatterit[i].childNodes[0].nodeValue,
+                                teatteriID:teatteriIDt[i].childNodes[0].nodeValue,
+                                aloitusaika:aloitusAjat[i].childNodes[0].nodeValue,
+                                lopetusaika:lopetusAjat[i].childNodes[0].nodeValue,
+                                pituus:pituudet[i].childNodes[0].nodeValue,
+                                ikärajaKuva:ikaRajaKuvat[i].childNodes[0].nodeValue,
+                                kuva:kuvaURLit[i].childNodes[0].nodeValue};
+                
+                var aloitusaika = new Date(elokuvatiedot.aloitusaika);
+                var lopetusaika = new Date(elokuvatiedot.lopetusaika);
+
+                if (menu.options[menu.selectedIndex].value == elokuvatiedot.teatteri) {
+                        data += '<tr>';
+                        data += '<td>' + elokuvatiedot.nimi + "<br>(" + elokuvatiedot.julkaisuvuosi + ")" + '</td>';
+                        data += '<td>' + elokuvatiedot.teatteri + "<br>(" + aloitusaika.toUTCString() + " - " + lopetusaika.toUTCString() + ")" + '</td>';
+                        data += '<td>' + elokuvatiedot.pituus + '</td>';
+                        data += '<td><img src="' + elokuvatiedot.ikärajaKuva + '"></td>';
+                        data += '<td><img src="' + elokuvatiedot.kuva + '"></td>';
+                        data += '<tr>';
+
+                        document.getElementById("taulukko").innerHTML = data;
+                } 
+            }
+            data += "</table>";
         }
     }
 }
 
-document.getElementById("infoBtn").addEventListener("click", function() {
+document.getElementById("dropMenu").addEventListener("change", function() {
     getInfo();
 });
-
-
